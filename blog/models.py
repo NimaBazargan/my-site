a725e45ab922da9b9a6fdb241dd7f389779f6ad5
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-08-08 17:59:12 +0330

    updated models
    updated post model and created contact model

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,12 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # image
+    # category
+    # author
+    published_date = models.DateTimeField(null=True)
+    views = models.BigIntegerField(default=0)
+    # tag
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
