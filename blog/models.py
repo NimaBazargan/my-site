a7d5388a927a99108515cade95f3d412055a69b9
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-08-09 11:04:34 +0330

    register Post model
    registered firs model on the site

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,6 @@ class Post(models.Model):
     status = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.id}"
