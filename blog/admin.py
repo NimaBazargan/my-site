2d1d68857aafb343d566a05736251b59735b7262
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-08-09 18:16:18 +0330

    make changes in admin panel
    created PostAdmin class in admin and made customize changes on admin panel in post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+# @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    # fields = ('title',)
+    # exclude = ('title',)
+    list_display = ('title' ,'views','status','published_date','created_date')
+    list_filter = ('status',)
+    ordering = ['created_date']
+    search_fields = ['title','content']
 
-# Register your models here.
+admin.site.register(Post,PostAdmin)
