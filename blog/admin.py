1fd5ee77d03078b229bf0e90987fee58ab266d93
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-08-11 15:21:53 +0330

    added category
    created Category model and added category field in Post model and needed pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -13,3 +13,4 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title','content']
 
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to='blog/', default= 'blog/default.jpg')
-    # category
+    category = models.ManyToManyField(Category)
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     published_date = models.DateTimeField(null=True)
     views = models.BigIntegerField(default=0)
