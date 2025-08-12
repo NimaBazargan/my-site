7c87b4d2aa3e095a55e4d2483aee1ea777601260
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-08-12 12:05:16 +0330

    updated blog
    updated category field and added prev and next post

--- blog/views.py
+++ blog/views.py
@@ -14,8 +14,20 @@ def single_view(request,pid):
     post = get_object_or_404(posts,id=pid)
     post.views += 1
     post.save()
+    index = list(posts).index(post)
+    prev_post = list(posts)[index-1]
+    len_list = len(list(posts))-1
+    if index != len_list:
+        next_post = list(posts)[index+1]
+    else:
+        next_post = list(posts)[0]
     context = {
         'post' : post,
+        'posts' : posts,
+        'index': index,
+        'len_list': len_list,
+        'prev_post': prev_post,
+        'next_post': next_post,
     }
     return render(request,'blog/blog-single.html',context)
 
