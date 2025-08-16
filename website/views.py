7cafe3d19dcff26ea05ad3971c2ad01c95f4f37d
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-08-16 20:59:00 +0330

    created forms
    created contact and newsletter forms

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from website.forms import ContactForm, NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request,'website/index.html')
@@ -8,4 +10,32 @@ def about_view(request):
     return render(request,'website/about.html')
 
 def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'success')
+        else:
+            messages.add_message(request,messages.ERROR,'error') 
+    form = ContactForm()       
     return render(request,'website/contact.html')
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+         return HttpResponseRedirect('/')
+
+# def test_view(request):
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Done')
+#         else:
+#             return render(request,'test.html',{'form':ContactForm(),'error':'not valid'})
+#     form = ContactForm()
+#  render(re)   return render(request,'test.html',{'form':form,'error':''})
