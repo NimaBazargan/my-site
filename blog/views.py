0551bf29c96fc85d0b160769b0e1cf07dcf7baee
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-08-16 12:23:47 +0330

    added pagination
    created paginator in blog home

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 def index_view(request,**kwargs):
     posts = Post.objects.filter(status = 1, published_date__lte=timezone.now())
@@ -11,6 +12,14 @@ def index_view(request,**kwargs):
     if request.method == 'GET':
         if q := request.GET.get('q'):
             posts = posts.filter(content__contains = q)
+    posts = Paginator(posts,2)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.page(posts.num_pages)
     context = {
         'posts': posts,
     }
